8112940747b39ee42e103ef5f8006457b1447d56
Diego <diego_leonel.94@hotmail.com>
2025-07-28 00:51:40 -0300

    Cambio de directorio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar/navbar';
+import Contenedor from './components/Contenedor/contenedor';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Contenedor 
+        mensaje="¡Bienvenido a GamerSell!"
+        nombre="Elden Ring"
+        genero="Acción-RPG"
+        precio={59.99}/>
+    </>
   );
 }
-
 export default App;
